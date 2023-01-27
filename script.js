87dd0cd64dffc461ffa2ea543559891cfda7e8bb
Ilyenna <jwelsh518@gmail.com>
2023-01-27 13:57:25 -0500

    Added some more stuff

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ const jennyMain = document.getElementById("jenny");
 const jovoMain = document.getElementById("jovo");
 const opheliaMain = document.getElementById("ophelia");
 
+const hollyName = document.getElementById("holly-name");
+const jennyName = document.getElementById("jenny-name");
+// const jovoMain = document.getElementById("jovo");
+// const opheliaMain = document.getElementById("ophelia");
+
 hollyMain.addEventListener("mouseenter", function() {
     hollyMain.style.opacity = 1;
+    hollyName.style.opacity = 1;
 });
 hollyMain.addEventListener("mouseleave", function() {
     hollyMain.style.opacity = 0.3;
+    hollyName.style.opacity = 0;
 });
 
 jennyMain.addEventListener("mouseenter", function() {
     jennyMain.style.opacity = 1;
+    jennyName.style.opacity = 1;
 });
 jennyMain.addEventListener("mouseleave", function() {
     jennyMain.style.opacity = 0.3;
+    jennyName.style.opacity = 0;
 });
 
 jovoMain.addEventListener("mouseenter", function() {
