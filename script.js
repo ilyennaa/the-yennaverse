87511fb2ee57c4f8f7b14efa91dfedd772422839
Ilyenna <jwelsh518@gmail.com>
2023-01-27 13:33:51 -0500

    Saving my progress

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("hi");
+const hollyMain = document.getElementById("holly");
+const jennyMain = document.getElementById("jenny");
+const jovoMain = document.getElementById("jovo");
+const opheliaMain = document.getElementById("ophelia");
+
+hollyMain.addEventListener("mouseenter", function() {
+    hollyMain.style.opacity = 1;
+});
+hollyMain.addEventListener("mouseleave", function() {
+    hollyMain.style.opacity = 0.3;
+});
+
+jennyMain.addEventListener("mouseenter", function() {
+    jennyMain.style.opacity = 1;
+});
+jennyMain.addEventListener("mouseleave", function() {
+    jennyMain.style.opacity = 0.3;
+});
+
+jovoMain.addEventListener("mouseenter", function() {
+    jovoMain.style.opacity = 1;
+});
+jovoMain.addEventListener("mouseleave", function() {
+    jovoMain.style.opacity = 0.3;
+});
+
+opheliaMain.addEventListener("mouseenter", function() {
+    opheliaMain.style.opacity = 1;
+});
+opheliaMain.addEventListener("mouseleave", function() {
+    opheliaMain.style.opacity = 0.3;
+});
